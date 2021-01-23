aaf42f44500bbeddaee2e4cee63f6bf5911fcde0
Awank <mas.awank100@gmail.com>
2021-01-23 16:49:11 +0700

    learn how to use while loop

--- looping.js
+++ looping.js
@@ -7,4 +7,16 @@ console.log(data)
 for (let i = 3; i < data.length; i++) {
   console.log(data[i])
 }
+
 // while
+let index = 0
+while (index < data.length) {
+  console.log('from while at index ' + index + ' with value = ' + data[index])
+  index = index + 1
+}
+
+let inc = 1
+while (inc < 10) {
+  console.log(inc)
+  inc++
+}
